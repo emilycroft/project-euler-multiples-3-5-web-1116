506a6ff286473992d80590cedaa53f864c7817c7
emilycroft <emilyhcroft@gmail.com>
2017-01-04 09:27:37 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,24 @@
 # Enter your procedural solution here!
+
+
+
+def collect_multiples(x)
+  n = 1
+  array = []
+  while n < x
+    if n % 5 == 0 || n % 3 == 0
+      array << n
+    end
+    n += 1
+  end
+  array
+end
+
+def sum_multiples(x)
+  total = 0
+  collect_multiples(x).each do |n|
+    total += n
+  end
+
+  total
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,31 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+  attr_accessor :x
+
+  def initialize(x)
+    @x = x
+  end
+
+  def collect_multiples
+    n = 1
+    array = []
+    while n < @x
+      if n % 5 == 0 || n % 3 == 0
+        array << n
+      end
+      n += 1
+    end
+    array
+  end
+
+  def sum_multiples
+    total = 0
+    collect_multiples.each do |n|
+      total += n
+    end
+
+    total
+  end
+
+end
